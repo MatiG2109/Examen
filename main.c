253514d643232ac604f670108f8804670239bd63
MatiG2109 <80924709+MatiG2109@users.noreply.github.com>
2021-07-10 08:21:58 +0000

    Avance 1, creación del menú

--- main.c
+++ main.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 
+typedef struct persona{
+
+  char nombre[20];
+  char rut[11];
+  int edad;
+
+}p;
+
 int main(void) {
   int op;
+  p * aux1;
+  p * aux2;
+  p * aux3;
  do{
     printf("1.  Ingresar  persona\n");
     printf("2.  Buscar persona por Nombre\n");
     printf("3.  Buscar persona por Rut\n");
     printf("4.  Exit\n");
-    printf("Escoger opcion correspondiente: ");
+    printf("Escoger una opcion: ");
     scanf("%i", &op);
 
     if((op > 4) || (op < 1)){
@@ -20,7 +31,12 @@ int main(void) {
   switch(op){
 
     case 1:
-    printf("OLa lo pibe");
+    printf("Ingrese Nombre: \n");
+    scanf("%s", aux1->nombre);
+    printf("Ingrese RUT: \n");
+    scanf("%s", aux2->rut);
+    printf("\nIngrese Edad : ");
+    scanf("%i", &aux3->edad);
     break;
     case 2:
 
