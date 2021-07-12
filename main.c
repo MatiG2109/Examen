17aa631ad12fc67f8a5a93cde629155e354adc40
MatiG2109 <80924709+MatiG2109@users.noreply.github.com>
2021-07-12 07:18:55 +0000

    Final. No se logro compilar de manera esperada:(

--- main.c
+++ main.c
@@ -11,10 +11,12 @@ typedef struct p{
 }p;
 
 int main() {
+  //Se crea una lista para así poder almacenar las personas
   List * lista = createList();
   int op;
   p * aux1;//Nueva Persona
   p * aux2;//Busqueda
+  p * aux3;
   p * top; //Recorrido
  do{
     printf("Escoger una opcion: \n");
@@ -25,6 +27,7 @@ int main() {
     scanf("%i", &op);
 
     if((op > 3) || (op < 1)){
+      printf("___________________________________\n\n");
       printf("Intente nuevamente\n");
     }
 
@@ -40,18 +43,21 @@ int main() {
     printf("Ingrese Nombre: \n");
     scanf("%s", aux1->nombre);
 
+    //Se coloca el aux TOP en el comienzo de la lista
     top=firstList(lista);
+   //Si no hay ningún elemento en la lista se ingresara aux1 (que contiene el nombre,rut y edad) al comienzo de la lista.
     if(top == NULL){
     pushFront(lista, aux1);
     }else{
+    //Si se encuentra alguna persona en la lista, se deberan comparar ambas personas atraves de sus edades para asi poder ordenar por edades.
       while(top != NULL){
         if(aux1->edad >= top->edad){
           top = nextList(lista);
         }else{
           if(nextList(lista) != NULL){
-            aux2 = nextList(lista);
+            aux3 = nextList(lista);
             pushCurrent(lista, aux1);
-            top = aux2;
+            top = aux3;
           }else{
             pushBack(lista, aux1);
            }
@@ -66,37 +72,44 @@ int main() {
     printf("Ingresar nombre a buscar: \n");
     char* nombre;
     scanf("\n%s", nombre);
-    aux1 = firstList(lista);
+    aux2 = firstList(lista);
+    //Se utiliza un do para comparar el nombre buscado en todos los elementos de la lista
     do{
-      if(aux1->nombre==nombre){
+      if(aux2->nombre==nombre){
         printf("Nombre \n");
         printf("%s\n", nombre);
         printf("Rut \n");
-        printf("%s \n", aux1->rut);
+        printf("%s \n", aux2->rut);
         printf("Edad \n");
-        printf("%i \n", aux1->edad);
+        printf("%i \n", aux2->edad);
       }
-      aux1 = nextList(lista);
-    }while(aux1!=NULL);
+      aux2 = nextList(lista);
+    }while(aux2!=NULL);
+
     break;
+
+
+
+
     case 3:
     printf("Ingresar nombre a buscar: \n");
     char* rut;
     scanf("\n%s", rut);
-    aux1 = firstList(lista);
+    aux2 = firstList(lista);
+    //Se utiliza la misma estructura que en la busqueda por nombre pero esta vez se comparara con el rut
     do{
-      if(aux1->rut==rut){
+      if(aux2->rut==rut){
         printf("Nombre \n");
-        printf("%s\n", aux1->nombre);
+        printf("%s\n", aux2->nombre);
         printf("Rut \n");
         printf("%s \n", rut);
         printf("Edad \n");
-        printf("%i \n", aux1->edad);
+        printf("%i \n", aux2->edad);
         
 
       }
-      aux1 = nextList(lista);
-    }while(aux1!=NULL);
+      aux2 = nextList(lista);
+    }while(aux2!=NULL);
     break;
   }
 }
